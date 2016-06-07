b79306d00aa0df4b7b77de7c6bd00ff6dfcd9fbb
Trey Reynolds <trey.reynolds@gmail.com>
2016-06-07 11:00:59 -0500

    Updated upsert SQL based on new table info

--- db/sql/servers/upsert.sql
+++ db/sql/servers/upsert.sql
@@ -1,20 +1,34 @@
-INSERT INTO ec2_servers AS ec2 (instance_id, availability_zone, status, state, system_status, created, last_updated)
+INSERT INTO ec2_servers AS ec2 (
+  instance_id,
+  availability_zone,
+  state,
+  public_ip,
+  public_url,
+  launch_time,
+  state_transition,
+  last_updated
+  )
 VALUES (${instance_id},
         ${availability_zone},
-        ${status},
         ${state},
-        ${system_status},
-        NOW(),
+        ${public_ip},
+        ${public_url},
+        ${launch_time},
+        ${state_transition},
         NOW()) 
 ON conflict (instance_id) DO
 UPDATE
 SET (availability_zone,
-     status,
-     STATE,
-     system_status,
+     state,
+     public_ip,
+     public_url,
+     launch_time,
+     state_transition,
      last_updated) = (${availability_zone},
-                      ${status},
                       ${state},
-                      ${system_status},
+                      ${public_ip},
+                      ${public_url},
+                      ${launch_time},
+                      ${state_transition},
                       NOW())
 WHERE ec2.instance_id = ${instance_id}
