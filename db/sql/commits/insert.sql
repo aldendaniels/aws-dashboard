4f6c70d3275c2c5c835de665204e9cb821f2a3e7
Trey Reynolds <trey.reynolds@gmail.com>
2016-06-07 15:46:28 -0500

    Added the concept of a configuration deployment repository to simplify for now.

--- db/sql/commits/insert.sql
+++ db/sql/commits/insert.sql
@@ -7,5 +7,6 @@ SELECT ${sha},
         ${repository},
         ${owner},
         NOW()
+FROM commits
 WHERE NOT EXISTS (SELECT sha from commits WHERE sha = ${sha})
 RETURNING sha, message, commit_author, author_name, avatar_url, repository, owner, created;
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db').db;
 var aws = require('../services/aws-service');
+var config = require('../config.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,7 +10,12 @@ router.get('/', function(req, res, next) {
 
     // Get the servers and commits from the database
     db.servers.all().then(function(servers){
-        return [servers,db.commits.all()];
+        var getCommits = db.commits.findByOwnerAndRepo({
+            owner: config.deploymentRepo.owner,
+            repository: config.deploymentRepo.repo
+        });
+
+        return [servers,getCommits];
 
     // The spread function lets us pass forward values
     // alongside our promises
